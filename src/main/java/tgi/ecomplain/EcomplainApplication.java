d11d81c617009af9b23c726d03bd8cf45974197b
Grzegorz Trukan <grzegorz.trukan@gmail.com>
2025-05-07 23:40:30 +0200

    Initial commit

---
+++ src/main/java/tgi/ecomplain/EcomplainApplication.java
@@ -0,0 +1,13 @@
+package tgi.ecomplain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomplainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomplainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tgi/ecomplain/EcomplainApplicationTests.java
@@ -0,0 +1,13 @@
+package tgi.ecomplain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomplainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
